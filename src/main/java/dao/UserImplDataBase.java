58ad2f41313e88dcfa1946d3ae38d8ca907c0a78
yolo <1035706563@qq.com>
2019-04-21 15:46:59 +0800

    工厂模式：读取配置创建userdao 暴露公开方法获取工厂对象和userdao 添加mysql高版本驱动

--- src/main/java/dao/UserImplDataBase.java
+++ src/main/java/dao/UserImplDataBase.java
@@ -3,7 +3,7 @@ package dao;
 import model.User;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
-import util.Utils2DB;
+import util.DBUtils;
 
 import java.sql.SQLException;
 import java.util.Date;
@@ -11,14 +11,14 @@ import java.util.Date;
 public class UserImplDataBase implements  UserDao {
     public User find(String username, String password) {
 
-        QueryRunner queryRunner = new QueryRunner(Utils2DB.getDataSource());
+        QueryRunner queryRunner = new QueryRunner(DBUtils.getDataSource());
 
         String sql = "SELECT * FROM user WHERE username=? AND password=?";
 
         try {
             User user = (User) queryRunner.query(sql, new BeanHandler(User.class), new Object[]{username, password});
 
-            return user == null ? null : user;
+            return user;
         } catch (SQLException e) {
             e.printStackTrace();
             throw new RuntimeException("登陆失败了！");
@@ -27,7 +27,7 @@ public class UserImplDataBase implements  UserDao {
 
     public void register(User user) {
 
-        QueryRunner queryRunner = new QueryRunner(Utils2DB.getDataSource());
+        QueryRunner queryRunner = new QueryRunner(DBUtils.getDataSource());
 
         String sql = "INSERT INTO user (id, username, password, email,birthday) VALUES (?,?,?,?,?);";
 
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -11,7 +11,7 @@
     <title>Title</title>
 </head>
 <body>
-<h1>这是首页！合并我吧</h1>
+<h1>这是首页！</h1>
 <a href="${pageContext.request.contextPath}/LoginUIServlet">登陆</a>
 <a href="${pageContext.request.contextPath}/RegisterUIServlet">注册</a>
 </body>
